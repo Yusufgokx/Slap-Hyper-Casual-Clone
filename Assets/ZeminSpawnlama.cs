739a472d6164f300322d87d553bbffa8ae438eab
hakansutcu <92602841+FurkanSutcuu@users.noreply.github.com>
2022-03-13 22:33:37 +0300

    gg
    gg

---
+++ Assets/ZeminSpawnlama.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZeminSpawnlama : MonoBehaviour
+{
+    public Transform spawnPosition;
+   
+     private void OnTriggerEnter(Collider other)
+     {
+         if (other.gameObject.CompareTag("Player"))
+         {
+             Instantiate(gameObject, spawnPosition.position, transform.rotation, null);
+         }
+     }
+}
