30d64f1061923f9f83226565eb8b60f85bbb8f3f
Yusufgokx <yusuf.fb.25@gmail.com>
2022-03-17 15:26:23 +0300

    gg
    gg

---
+++ Assets/Obstacle/ObstacleScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ObstacleScript : MonoBehaviour
+{
+   
+  
+    public float zaman;
+  
+  
+    void Start()
+    {
+        ObstacleMove();
+        
+    }
+
+ 
+    void Update()
+    {
+
+       transform.Rotate(Vector3.forward);
+    }
+    public void ObstacleMove()
+    {
+        //transform.DOMove(Hedef.position, zaman).OnComplete(() => geriGel());
+        transform.DOMove(new Vector3 (transform.position.x+5,transform.position.y,transform.position.z), zaman).OnComplete(() => transform.DOMove( new Vector3(transform.position.x-5,transform.position.y,transform.position.z) , 1).OnComplete(() => ObstacleMove())); //
+    }
+  /*public void geriGel()
+    {
+        transform.DOMoveX(-7, 5);
+    }*/
+}
+
